915b96be940198ac8e7043568a5058552122e3a5
david444456 <david-toranzo-@hotmail.com>
2023-01-06 23:47:54 -0300

    [A] add vector 3 SO event

---
+++ Runtime/Scripts/Vector3EventScriptableObject.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjects.Event
+{
+    [CreateAssetMenu(fileName = "Vector3EventSO", menuName = "ScriptableObjects/Events/Vector3", order = 0)]
+    public class Vector3EventScriptableObject : TypeEventScriptableObject<Vector3>
+    {
+
+    }
+}
