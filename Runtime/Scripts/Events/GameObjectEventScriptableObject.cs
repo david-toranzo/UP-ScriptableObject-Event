3348ce440f6387183d66634aa358efe4ae51a45f
david444456 <david-toranzo-@hotmail.com>
2023-01-08 11:17:44 -0300

    [A] add game object event so

---
+++ Runtime/Scripts/Events/GameObjectEventScriptableObject.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjects.Event
+{
+    [CreateAssetMenu(fileName = "GameObjectEventSO", menuName = "ScriptableObjects/Events/GameObject", order = 0)]
+    public class GameObjectEventScriptableObject : TypeEventScriptableObject<GameObject>
+    {
+
+    }
+}
