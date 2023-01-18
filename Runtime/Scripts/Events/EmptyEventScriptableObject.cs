056879fb0c8cdc0429b7d111bc685adb82206908
david444456 <david-toranzo-@hotmail.com>
2023-01-18 14:19:05 -0300

    [C] changed empty event

--- Runtime/Scripts/Events/EmptyEventScriptableObject.cs
+++ Runtime/Scripts/Events/EmptyEventScriptableObject.cs
@@ -3,9 +3,14 @@ using UnityEngine;
 
 namespace ScriptableObjects.Event
 {
-    [CreateAssetMenu(fileName = "EventSO", menuName = "ScriptableObjects/Events/Empy", order = 0)]
+    [CreateAssetMenu(fileName = "EventSO", menuName = "ScriptableObjects/Events/Empty", order = 0)]
     public class EmptyEventScriptableObject : ScriptableObject, IEmptyEvent
     {
         public Action OnEvent { get; set; }
+
+        public void InvokeEvent()
+        {
+            OnEvent?.Invoke();
+        }
     }
 }
