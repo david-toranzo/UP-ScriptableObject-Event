17964c3606980af19c561e62c6fffeb34577b602
david444456 <david-toranzo-@hotmail.com>
2023-05-07 22:09:51 -0300

    [A] add event data type

---
+++ Runtime/Scripts/DataEvent/BoolDataEventScriptableObject.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjects.Data
+{
+    [CreateAssetMenu(fileName = "BoolDataEventSO", menuName = "ScriptableObjects/Events/Data/Bool", order = 0)]
+    public class BoolDataEventScriptableObject : TypeEventDataScriptableObject<bool>
+    {
+
+    }
+}
---
+++ Runtime/Scripts/DataEvent/TypeEventDataScriptableObject.cs
@@ -0,0 +1,15 @@
+﻿using ScriptableObjects.Event;
+using System;
+
+namespace ScriptableObjects.Data
+{
+    public abstract class TypeEventDataScriptableObject<Type> : TypeDataScriptableObject<Type>, IEventType<Type>
+    {
+        public Action<Type> OnTypeEvent { get; set; }
+
+        public void InvokeEventType(Type data)
+        {
+            OnTypeEvent?.Invoke(data);
+        }
+    }
+}
