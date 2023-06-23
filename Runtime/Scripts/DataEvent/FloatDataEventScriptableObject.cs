734e5f4bc88fce7778ee050be06e377754847544
david444456 <david-toranzo-@hotmail.com>
2023-06-23 00:32:59 -0300

    [A] add float events and data

---
+++ Runtime/Scripts/DataEvent/FloatDataEventScriptableObject.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjects.Data
+{
+    [CreateAssetMenu(fileName = "FloatDataEventSO", menuName = "ScriptableObjects/Events/Data/Float", order = 0)]
+    public class FloatDataEventScriptableObject : TypeEventDataScriptableObject<float>
+    {
+
+    }
+}
---
+++ Runtime/Scripts/Events/FloatEventScriptableObject.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjects.Event
+{
+    [CreateAssetMenu(fileName = "FloatEventSO", menuName = "ScriptableObjects/Events/Float", order = 0)]
+    public class FloatEventScriptableObject : TypeEventScriptableObject<float>
+    {
+
+    }
+}
